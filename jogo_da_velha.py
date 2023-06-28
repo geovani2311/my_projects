9997fec56a8a46cc6136b48daa8b916bc9786916
geovani2311 <geovani.cardoso1@outlook.com>
2023-06-27 23:37:22 -0300

    changing the list

--- jogo_da_velha.py
+++ jogo_da_velha.py
@@ -2,9 +2,9 @@ import random
 import time
 
 # VARIAVEIS GLOBAIS
-lista = [" "," "," "," "," "," "," "," "," "]
 
 def jogo():
+    lista = [[" "," "," "],[" "," "," "],[" "," "," "]]
     print("Bem vindo ao jogo da velha\n")
     print("Escolha a posição da coluna e linha para assinalar, sendo 0 a primeira coluna e 0 a primeira linha")
     time.sleep(1)
@@ -15,92 +15,28 @@ def jogo():
     time.sleep(1)
     print("1\n")
     
-    print(lista[0:3])
-    print(lista[3:6])
-    print(lista[6:9])
-    
+    print("  " + lista[0][0] +" ║ "+ lista[0][1] +" ║ "+ lista[0][2])
+    print("═════════════")
+    print("  " + lista[1][0] +" ║ "+ lista[1][1] +" ║ "+ lista[1][2])
+    print("═════════════")
+    print("  " + lista[2][0] +" ║ "+ lista[2][1] +" ║ "+ lista[2][2])
+
+#DEFINIR ESSA AÇÃO COMO CLASSE USUARIO
     coluna = int(input("Digite a coluna"))
-    linha = int(input("Digite a linha"))
-    jogada = (str(coluna) +","+ str(linha))
+    linha =  int(input("Digite a linha"))
+    
+    lista = lista[coluna][linha]
+    
+    print("  " + lista[0][0] +" ║ "+ lista[0][1] +" ║ "+ lista[0][2])
+    print("═════════════")
+    print("  " + lista[1][0] +" ║ "+ lista[1][1] +" ║ "+ lista[1][2])
+    print("═════════════")
+    print("  " + lista[2][0] +" ║ "+ lista[2][1] +" ║ "+ lista[2][2])
 
-    if jogada == "0,0":
-        usuario_escolhe = "X"
-        lista[0] = usuario_escolhe
-        print()
-        print(lista[0:3])
-        print(lista[3:6])
-        print(lista[6:9])
-
-        maquina()
-
-    elif jogada == "0,1":
-        usuario_escolhe = "X"
-        lista[1] = usuario_escolhe
-        print()
-        print(lista[0:3])
-        print(lista[3:6])
-        print(lista[6:9])
-
-    elif jogada == "0,2":
-        usuario_escolhe = "X"
-        lista[2] = usuario_escolhe
-        print()
-        print(lista[0:3])
-        print(lista[3:6])
-        print(lista[6:9]) 
-
-    elif jogada == "1,0":
-        usuario_escolhe = "X"
-        lista[3] = usuario_escolhe
-        print()
-        print(lista[0:3])
-        print(lista[3:6])
-        print(lista[6:9])  
-
-    elif jogada == "1,1":
-        usuario_escolhe = "X"
-        lista[4] = usuario_escolhe
-        print()
-        print(lista[0:3])
-        print(lista[3:6])
-        print(lista[6:9])  
-
-    elif jogada == "1,2":
-        usuario_escolhe = "X"
-        lista[5] = usuario_escolhe
-        print()
-        print(lista[0:3])
-        print(lista[3:6])
-        print(lista[6:9])  
-
-    elif jogada == "2,0":
-        usuario_escolhe = "X"
-        lista[6] = usuario_escolhe
-        print()
-        print(lista[0:3])
-        print(lista[3:6])
-        print(lista[6:9])  
-
-    elif jogada == "2,1":
-        usuario_escolhe = "X"
-        lista[7] = usuario_escolhe
-        print()
-        print(lista[0:3])
-        print(lista[3:6])
-        print(lista[6:9])  
-
-    elif jogada == "2,2":
-        usuario_escolhe = "X"
-        lista[8] = usuario_escolhe
-        print()
-        print(lista[0:3])
-        print(lista[3:6])
-        print(lista[6:9])     
-
-    else:
-        print("jogada invalida, recomece o jogo")
+jogo()
 
 
+#DEFINIR CLASSE PARA MAQUINA NA QUAL PEGA ATRIBUTOS E MÉTODOS DO USUARIO
 def maquina():
     numero_aleatorio = 1 
     while numero_aleatorio in lista:
@@ -110,6 +46,3 @@ def maquina():
         print(lista[0:3])
         print(lista[3:6])
         print(lista[6:9])
-
-
-jogo()
