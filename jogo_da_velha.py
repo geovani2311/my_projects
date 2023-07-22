ca7cf1e0753174db22087a0606b9fabf3f33fc05
geovani2311 <geovani.cardoso1@outlook.com>
2023-07-22 19:36:43 -0300

    finalizando jogo

--- jogo_da_velha.py
+++ jogo_da_velha.py
@@ -3,6 +3,7 @@ import time
 import os
 
 class tictactoe:
+    #inicializa 
     def __init__(self):
         self.reset() 
         print("Bem vindo ao jogo da velha\n")
@@ -16,39 +17,35 @@ class tictactoe:
         print("1\n")
         
     #printa o tabuleiro
-    def board(self):
+    def print_board(self):
         print("-------------")
-        print("  " + self.lista[0][0] +" ║ "+ self.lista[0][1] +" ║ "+ self.lista[0][2])
+        print("  " + self.board[0][0] +" ║ "+ self.board[0][1] +" ║ "+ self.board[0][2])
         print("═════════════")
-        print("  " + self.lista[1][0] +" ║ "+ self.lista[1][1] +" ║ "+ self.lista[1][2])
+        print("  " + self.board[1][0] +" ║ "+ self.board[1][1] +" ║ "+ self.board[1][2])
         print("═════════════")
-        print("  " + self.lista[2][0] +" ║ "+ self.lista[2][1] +" ║ "+ self.lista[2][2])
+        print("  " + self.board[2][0] +" ║ "+ self.board[2][1] +" ║ "+ self.board[2][2])
         print("-------------")
 
     #reseta o jogo
     def reset(self):
-        self.lista = [[" "," "," "],[" "," "," "],[" "," "," "]]
+        self.board = [[" "," "," "],[" "," "," "],[" "," "," "]]
         self.done = ""
 
     #verificar quem ganhou ou se teve empate
     def check_win(self):
         dict_win = {}
-
         for i in ["X", "O"]:
-
             #horizontal
-            dict_win[i] = (self.lista[0][0] == self.lista[0][1] == self.lista[0][2] == i)
-            dict_win[i] = (self.lista[1][0] == self.lista[1][1] == self.lista[1][2] == i) or dict_win[i]
-            dict_win[i] = (self.lista[2][0] == self.lista[2][1] == self.lista[2][2] == i) or dict_win[i]
-
-            #vertical
-            dict_win[i] = (self.lista[0][0] == self.lista[1][0] == self.lista[2][0] == i) or dict_win[i]
-            dict_win[i] = (self.lista[0][1] == self.lista[1][1] == self.lista[2][1] == i) or dict_win[i]
-            dict_win[i] = (self.lista[0][2] == self.lista[1][2] == self.lista[2][2] == i) or dict_win[i]
-
-            #diagonal
-            dict_win[i] = (self.lista[0][0] == self.lista[1][1] == self.lista[2][2] == i) or dict_win[i]
-            dict_win[i] = (self.lista[0][2] == self.lista[1][1] == self.lista[2][0] == i) or dict_win[i]
+            dict_win[i] = (self.board[0][0] == self.board[0][1] == self.board[0][2] == i)
+            dict_win[i] = (self.board[1][0] == self.board[1][1] == self.board[1][2] == i) or dict_win[i]
+            dict_win[i] = (self.board[2][0] == self.board[2][1] == self.board[2][2] == i) or dict_win[i]
+            #verboard
+            dict_win[i] = (self.board[0][0] == self.board[1][0] == self.board[2][0] == i) or dict_win[i]
+            dict_win[i] = (self.board[0][1] == self.board[1][1] == self.board[2][1] == i) or dict_win[i]
+            dict_win[i] = (self.board[0][2] == self.board[1][2] == self.board[2][2] == i) or dict_win[i]
+            #diaboard
+            dict_win[i] = (self.board[0][0] == self.board[1][1] == self.board[2][2] == i) or dict_win[i]
+            dict_win[i] = (self.board[0][2] == self.board[1][1] == self.board[2][0] == i) or dict_win[i]
 
         if dict_win["X"]:
             self.done = "X"
@@ -57,25 +54,22 @@ class tictactoe:
         elif dict_win["O"]:
             print("O venceu!")
             return
-        else:
-            print("Empate")
         
         #chegando se todos campos da lista estão preenchidos 
         c = 0 
         for i in range(3):
             for j in range(3):
-                if self.lista[i][j] != "":
+                if self.board[i][j] != "":
                     c = c + 1 
                     break
-
         if c == 0:
             self.done = "d"
             print("Empate!")
             return
-        
+
+    #jogadas do jogador e prevenindo do jogador de fazer jogadas inválidas    
     def player_move(self):
-        invalid_move = True    
-        #prevenindo jogador de fazer jogadas inválidas 
+        invalid_move=True        
         while invalid_move:
             try:
                 print("Digite linha: ")
@@ -86,41 +80,41 @@ class tictactoe:
                 if linha > 2 or linha < 0 or coluna > 2 or coluna < 0:
                     print("Coordenadas inválidas")
 
-                if self.board[x][y] != " ":
+                if self.board[linha][coluna] != " ":
                     print("Posição já preenchida.")
                     continue
-
             except Exception as e:
                 print(e)
                 continue
 
             invalid_move = False
-            self.lista[X][Y] = "X"
+            self.board[linha][coluna] = "X"
 
+
+    #jogadas do computador
     def computer_move(self):
         list_moves = []
-
         for i in range(3):
             for j in range(3):
                 if self.board[i][j] == " ":
                     list_moves.append((i, j))
+        if len(list_moves) > 0:
+            linha, coluna = random.choice(list_moves)
+            self.board[linha][coluna] = "O" 
 
-            if len(list_moves) > 0:
-                x, y = random.choice(list_moves)
-                self.board[x][y] = "O" 
+
+tictactoe = tictactoe()
+next = 0 
 
 while next == 0:
-    os.system("cls")
-    tictactoe.print_lista()
+    tictactoe.print_board()
     while tictactoe.done == "":
         tictactoe.player_move()
         tictactoe.computer_move()
         os.system("cls")
-        tictactoe.print_lista()
+        tictactoe.print_board()
         tictactoe.check_win()
-
     print("digite 1 para sair do jogo ou qualquer tecla para jogar novamente")
-
     next = int(input())
     if next == 1:
         break
